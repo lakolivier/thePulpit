5a670634cc64daba10f31675bbd062f6de6eeb06
lakolivier <lakolivier@gmail.com>
2020-02-10 20:40:10 -0500

    _GET

--- openSermon.php
+++ openSermon.php
@@ -10,8 +10,10 @@ $link = new mysqli ($servername, $username, $password, $dbname);
 if($link -> connect_error){
     die("Connection failed: " . $link -> connect_error);
 }
+//get variables from url using http _get
+$retrieveId = $_GET['sermonId'];
 //select from db REQUIRES INPUT OF SERMONID
-$sql = "SELECT title, verse, dateOf, vidLink, imgLink, numImgs FROM Sermons WHERE sermonId = 1";
+$sql = "SELECT title, verse, dateOf, vidLink, imgLink, numImgs FROM Sermons WHERE sermonId = $retrieveId";
 $result = $link->query($sql);
 $test = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
