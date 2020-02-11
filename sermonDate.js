e939058ed9fe511d2b3612d4ea24c3c239d8482b
lakolivier <lakolivier@gmail.com>
2020-02-10 20:32:39 -0500

    text-decoration-none

--- sermonDate.js
+++ sermonDate.js
@@ -14,7 +14,7 @@ $(function() {
             //if year of current sermon is the same year, append title and date
             if (jsArr[i]["dateOf"].getFullYear() == holdDate) {
                     $("#" + holdDate).append('<div class = "card my-1 bg-light border-0" id = "' + holdDate + i + '"><div class = "card-body"></div></div>');
-                    $("#" + holdDate + i).children("div.card-body").append('<h4 class = "card-title text-primary"><a href = "openSermon.html?id=' + jsArr[i]["sermonId"] + '">' + jsArr[i]["title"] + '</a></h4>');
+                    $("#" + holdDate + i).children("div.card-body").append('<h4 class = "card-title text-primary"><a href = "openSermon.html?id=' + jsArr[i]["sermonId"] + '" class = "text-decoration-none">' + jsArr[i]["title"] + '</a></h4>');
                     $("#" + holdDate + i).children("div.card-body").append('<p class = "card-text text-muted">' + jsArr[i]["dateOf"].toDateString() + "</p>");
             }
             //else change holdDate to new year, make new div, append current sermon info
@@ -23,7 +23,7 @@ $(function() {
                 $("#" + lastDate).after('<div class = "card col-xl-12 my-3 border-0 divYear" id = "' + holdDate + '"></div>');
                 $("#" + holdDate).append('<h2 class = "card-title">' + holdDate + "</h2>")
                 $("#" + holdDate).append('<div class = "card my-1 bg-light border-0" id = "' + holdDate + i + '"><div class = "card-body"></div></div>');
-                $("#" + holdDate + i).children("div.card-body").append('<h4 class = "card-title text-primary"><a href = "openSermon.html?id=' + jsArr[i]["sermonId"] + '">' + jsArr[i]["title"] + '</a></h4>');
+                $("#" + holdDate + i).children("div.card-body").append('<h4 class = "card-title text-primary"><a href = "openSermon.html?id=' + jsArr[i]["sermonId"] + '" class = "text-decoration-none">' + jsArr[i]["title"] + '</a></h4>');
                 $("#" + holdDate + i).children("div.card-body").append('<p class = "card-text text-muted">' + jsArr[i]["dateOf"].toDateString() + "</p>");
                 lastDate = holdDate;    
             }
