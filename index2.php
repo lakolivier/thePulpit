e1cee1abbf5510d4f5b00df7da318e218c6549a7
lakolivier <lakolivier@gmail.com>
2020-02-12 14:44:32 -0500

    added randomizer to front page

--- index2.php
+++ index2.php
@@ -10,33 +10,13 @@ $link = new mysqli ($servername, $username, $password, $dbname);
 if($link -> connect_error){
     die("Connection failed: " . $link -> connect_error);
 }
-//get variables from url using http _get
-$retrieveUrl = $_GET['sermonUrl'];
-//parse URL to isolate http parameters
-$parsedUrl = parse_url($retrieveUrl);
-$queryString = $parsedUrl["query"];
-parse_str($queryString, $queryStrings);
-//pick random id from array
-$randomSermon = rand(1, count($queryStrings))
-//select from db and put into associative array
-$sql = "SELECT title, dateOf, vidLink FROM Sermons WHERE sermonId = " . $randomSermon;
+//select from db REQUIRES INPUT OF SERMONID
+$sql = "SELECT title, dateOf, vidLink FROM Sermons WHERE sermonId = " . rand(1, count($queryStrings));
 $result = $link->query($sql);
 $test = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
-/*
-//returns json object assigned value
-$result = [
-    "title" => "The Power of Hope",
-    "verse" => "John 5: 1-9",
-    "dateOf" => "1994-02-06",
-    "vidLink" => "CiOil1_VBUU",
-    "imgLink" => "Feb6_94",
-    "numImgs" => 3
-];
-*/
-
 if (!empty($test)){
-    echo json_encode($test[0]);
+    echo json_encode($test);
 }
 else{
     echo json_encode([]);
