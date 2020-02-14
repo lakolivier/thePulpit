8cca3dd8297fddb99c23af6c218e529ce6c88914
lakolivier <lakolivier@gmail.com>
2020-02-14 17:06:18 -0500

    started lcv at 1 instead

--- justDate.js
+++ justDate.js
@@ -4,14 +4,14 @@ $(function() {
         //convert json to js object
         var jsArr = JSON.parse(jsonObj);
         //basically plan: select ALL from database in php file, then convert to big ol' json object, then loop thru to display.
-        /*let holdDate = new Date(jsArr[0]["YEAR(dateOf)"]).getFullYear();
+        let holdDate = new Date(jsArr[0]["YEAR(dateOf)"]).getFullYear();
         let lastDate = holdDate;
         $("nav").after('<div class = "container" id = "allYears"></div>');
         $("#allYears").append('<div class = "card col-xl-12 my-3 border-0 divYear" id = "' + holdDate + '"></div>');
-        $("#" + holdDate).append('<h2 class = "card-title"><a href = "sermonDate.html#'+ holdDate + '" class = "text-decoration-none">' + holdDate + "</a></h2>");*/
-        for (let i = 0; i < $(jsArr).length; i++) {
+        $("#" + holdDate).append('<h2 class = "card-title"><a href = "sermonDate.html#'+ holdDate + '" class = "text-decoration-none">' + holdDate + "</a></h2>");
+        for (let i = 1; i < $(jsArr).length; i++) {
             jsArr[i]["YEAR(dateOf)"] = new Date(jsArr[i]["YEAR(dateOf)"]);
-            holdDate = jsArr[i]["YEAR(dateOf)"].getFullYear();
+            let holdDate = jsArr[i]["YEAR(dateOf)"].getFullYear();
             $("#" + lastDate).after('<div class = "card col-xl-12 my-3 border-0 divYear" id = "' + holdDate + '"></div>');
             $("#" + holdDate).append('<h2 class = "card-title"><a href = "sermonDate.html#'+ holdDate + '" class = "text-decoration-none">' + holdDate + "</a></h2>");
             lastDate = holdDate;
