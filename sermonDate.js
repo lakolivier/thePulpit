e3a873ca7ef6c682b10df402a2f2c45a6f400fb1
lakolivier <lakolivier@gmail.com>
2020-02-16 22:30:17 -0500

    error checking

--- sermonDate.js
+++ sermonDate.js
@@ -47,6 +47,6 @@ $(function() {
     //pull passed variable from URL, scroll into view
     let currentUrl = window.location.href;
     let splitUrl = currentUrl.split("#");
-    console.log(document.getElementById("2003"));
+    console.log(document.getElementById("#2003"));
     //targetId.scrollIntoView();
 });
