04771217cbf6694fb0701929d29a553cc7a3cb01
lakolivier <lakolivier@gmail.com>
2020-02-16 21:00:31 -0500

    fixed anchor tags

--- sermonDate.js
+++ sermonDate.js
@@ -10,7 +10,7 @@ $(function() {
         let holdDate = new Date(jsArr[0]["dateOf"]).getUTCFullYear();
         let lastDate = holdDate;
         $("nav").after('<div class = "container" id = "allYears"></div>');
-        $("#allYears").append('<div class = "card col-xl-12 my-3 border-0 divYear" name = "' + holdDate + '"></div>');
+        $("#allYears").append('<div class = "card col-xl-12 my-3 border-0 divYear" id = "' + holdDate + '" name = "' + holdDate + '"></div>');
         $("#" + holdDate).append('<h2 class = "card-title">' + holdDate + "</h2>")
         for (let i = 0; i < $(jsArr).length; i++) {
             jsArr[i]["dateOf"] = new Date(jsArr[i]["dateOf"]);
@@ -29,7 +29,7 @@ $(function() {
             //else change holdDate to new year, make new div, append current sermon info
             else {
                 holdDate = jsArr[i]["dateOf"].getUTCFullYear();
-                $("#" + lastDate).after('<div class = "card col-xl-12 my-3 border-0 divYear" name = "' + holdDate + '"></div>');
+                $("#" + lastDate).after('<div class = "card col-xl-12 my-3 border-0 divYear" id = "' + holdDate + '" name = "' + holdDate + '"></div>');
                 $("#" + holdDate).append('<h2 class = "card-title">' + holdDate + "</h2>")
                 $("#" + holdDate).append('<div class = "card my-1 bg-light border-0" id = "' + holdDate + i + '"><div class = "card-body"></div></div>');
                 //if sermon has a title, append ELSE display placeholder
