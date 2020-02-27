88412a25efc41eb8253c431b1ff53ca1b66167c9
lakolivier <lakolivier@gmail.com>
2020-02-27 16:21:55 -0500

    added readings page

---
+++ justTags.js
@@ -0,0 +1,7 @@
+$(function() {
+    $.get("justTags.php", function(jsonObj) {
+        //convert json to js object
+        var jsArr = JSON.parse(jsonObj);
+        
+    });
+});
---
+++ justTags.php
@@ -0,0 +1,24 @@
+<?php
+//login information for mysql database
+$servername = "rectory.gilbowen.dreamhosters.com";
+$username = "gilbowen";
+$password = "!bowen19";
+$dbname = "rectory6";
+//start connection
+$link = new mysqli ($servername, $username, $password, $dbname);
+//check connection
+if($link -> connect_error){
+    die("Connection failed: " . $link -> connect_error);
+}
+//select from db REQUIRES INPUT OF SERMONID
+$sql = "SELECT DISTINCT dateOf FROM Sermons ORDER BY dateOf";
+$result = $link->query($sql);
+$test = mysqli_fetch_all($result, MYSQLI_ASSOC);
+//if pulled data then echo to js file
+if (!empty($test)){
+    echo json_encode($test);
+}
+else{
+    echo json_encode([]);
+}
+?>
