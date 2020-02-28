1fadc5330ef48215fc9fc7e3ba14dba4c004f2d5
lakolivier <lakolivier@gmail.com>
2020-02-28 11:49:36 -0500

    adjusted readings

--- openReading.js
+++ openReading.js
@@ -10,9 +10,5 @@ $(function(){
             $("#sermonTitle").html("Beyond Myself: Faith Hope Live");
             $("#bookBox").attr("src", "https://drive.google.com/file/d/1nvNQ1oXZQ3JVIMQ3QiQACglIl4go4b8e/preview");
             break;
-        default:
-            $("#sermonTitle").html("Beyond Myself: Faith Hope Live");
-            $("#bookBox").attr("src", "https://drive.google.com/file/d/1nvNQ1oXZQ3JVIMQ3QiQACglIl4go4b8e/preview");
-            break;
     }
 });
