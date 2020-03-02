229aafd1830ac76fee6b0b42f74735f344048d15
lakolivier <lakolivier@gmail.com>
2020-03-01 23:19:29 -0500

    added tags page

--- justTags.js
+++ justTags.js
@@ -2,11 +2,11 @@ $(function() {
     $.get("justTags.php", function(jsonObj) {
         //convert json to js object
         var jsArr = JSON.parse(jsonObj);
-        $("#allTags").append('div class = "card col-8 my-3 border-0 mx-auto" id = "' + jsArr[0]["tagName"] + '"></div>');
-        $("#" + jsArr[0]["tagName"]).append('h2 class = "card-title margin-0 border-bottom"><a href = "sermonTags.html#' + jsArr[0]["tagName"] + '" class = "text-dark text-decoration-none">' + jsArr[0]["tagName"] + '</a></h2>');
+        $("#allTags").append('<div class = "card col-8 my-3 border-0 mx-auto" id = "' + jsArr[0]["tagName"] + '"></div>');
+        $("#" + jsArr[0]["tagName"]).append('<h2 class = "card-title margin-0 border-bottom"><a href = "sermonTags.html#' + jsArr[0]["tagName"] + '" class = "text-dark text-decoration-none">' + jsArr[0]["tagName"] + '</a></h2>');
         for (let i = 1; i < $(jsArr).length; i++) {
-            $("#" + jsArr[i - 1]["tagName"]).after('div class = "card col-8 my-3 border-0 mx-auto" id = "' + jsArr[i]["tagName"] + '"></div>');
-            $("#" + jsArr[i]["tagName"]).append('h2 class = "card-title margin-0 border-bottom"><a href = "sermonTags.html#' + jsArr[i]["tagName"] + '" class = "text-dark text-decoration-none">' + jsArr[i]["tagName"] + '</a></h2>');
+            $("#" + jsArr[i - 1]["tagName"]).after('<div class = "card col-8 my-3 border-0 mx-auto" id = "' + jsArr[i]["tagName"] + '"></div>');
+            $("#" + jsArr[i]["tagName"]).append('<h2 class = "card-title margin-0 border-bottom"><a href = "sermonTags.html#' + jsArr[i]["tagName"] + '" class = "text-dark text-decoration-none">' + jsArr[i]["tagName"] + '</a></h2>');
         }
     });
 });
