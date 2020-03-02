35625da18425591e59f52b5051fdab23ce4d0582
lakolivier <lakolivier@gmail.com>
2020-03-02 14:19:21 -0500

    intentional syntax

--- sermonDate.php
+++ sermonDate.php
@@ -13,33 +13,8 @@ if($link -> connect_error){
 }
 //select from db REQUIRES INPUT OF SERMONID
 $sql = "SELECT title, dateOf, sermonId FROM Sermons ORDER BY dateOf";
-$sql3 = "SELECT title, dateOf, vidLink, imgLink, sermonId FROM Sermons ORDER BY RAND() LIMIT 9";
-echo $sql3;
-$result3 = $link->query($sql3);
 $result = $link->query($sql);
-//echo $result;
 $test = mysqli_fetch_all($result, MYSQLI_ASSOC);
-//echo $test;
-/*
-$result = [
-    "title" => "Who Are The Powerful?",
-    //THIS NEEDS TO BE PASSED AS STRING, NOT DATE
-    "dateOf" => "1995-12-17",
-];
-$result2 = [
-    "title" => "The Three Rs of Real Faith",
-    "dateOf" => "1995-12-10",
-];
-$result3 = [
-    "title" => "Salvation in Self-forgetfulness",
-    "dateOf" => "1998-04-15",
-];
-
-$array = [];
-$array[] = $result;
-$array[] = $result2;
-$array[] = $result3;
-*/
 
 if (!empty($test)){
     echo json_encode($test);
