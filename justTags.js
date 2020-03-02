825169f2a104686460805bfab9f2b9bc5c3c56da
lakolivier <lakolivier@gmail.com>
2020-03-01 23:23:23 -0500

    added tags page

--- justTags.js
+++ justTags.js
@@ -8,6 +8,7 @@ $(function() {
         for (let i = 1; i < $(jsArr).length; i++) {
             $("#" + jsArr[i - 1]["tagName"]).after('<div class = "card col-8 my-3 border-0 mx-auto" id = "' + jsArr[i]["tagName"] + '"></div>');
             $("#" + jsArr[i]["tagName"]).append('<h2 class = "card-title margin-0 border-bottom"><a href = "sermonTags.html#' + jsArr[i]["tagName"] + '" class = "text-dark text-decoration-none">' + jsArr[i]["tagName"] + '</a></h2>');
+            console.log (i);
         }
     });
 });
