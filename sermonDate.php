0269e9eeb1a52bceaa0b4557f553b4797bf67c2d
lakolivier <lakolivier@gmail.com>
2020-03-02 14:21:53 -0500

    fuckery

--- sermonDate.php
+++ sermonDate.php
@@ -6,14 +6,14 @@ $password = "!bowen19";
 $dbname = "rectory6";
 //start connection
 $link = new mysqli ($servername, $username, $password, $dbname);
-echo $link;
 //check connection
 if($link -> connect_error){
     die("Connection failed: " . $link -> connect_error);
 }
-//select from db REQUIRES INPUT OF SERMONID
-$sql = "SELECT title, dateOf, sermonId FROM Sermons ORDER BY dateOf";
-$result = $link->query($sql);
+//select from db 
+//$sql2 = "SELECT TOP 9 title, dateOf, vidLink FROM Sermons ORDER BY NEWID()";
+$sql3 = "SELECT title, dateOf, vidLink, imgLink, sermonId FROM Sermons ORDER BY RAND() LIMIT 9";
+$result = $link->query($sql3);
 $test = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
 if (!empty($test)){
