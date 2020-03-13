3c669077f934a0a35ddd1da5dc2e511e44752b03
lakolivier <lakolivier@gmail.com>
2020-03-13 16:22:55 -0400

    nick testing

--- sermonDate.php
+++ sermonDate.php
@@ -12,7 +12,8 @@ if($link -> connect_error){
 }
 //select from db 
 //$sql2 = "SELECT TOP 9 title, dateOf, vidLink FROM Sermons ORDER BY NEWID()";
-$sql3 = "SELECT title, dateOf, vidLink, imgLink, sermonId FROM Sermons";
+$sql3 = "SELECT title, dateOf, vidLink, imgLink, sermonId FROM Sermons ORDER BY RAND() LIMIT 9";
+//$sql3 = "SELECT title, dateOf, sermonId FROM Sermons ORDER BY dateOf";
 $result = $link->query($sql3);
 $test = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
