0022954db740508063647ccc3c17675e6c835c29
lakolivier <lakolivier@gmail.com>
2020-03-13 16:33:45 -0400

    fixing nonsense

--- sermonDate.php
+++ sermonDate.php
@@ -12,17 +12,9 @@ if($link -> connect_error){
 }
 //select from db 
 //$sql2 = "SELECT TOP 9 title, dateOf, vidLink FROM Sermons ORDER BY NEWID()";
-$sql3 = "SELECT title, dateOf, vidLink, imgLink, sermonId FROM Sermons ORDER BY RAND() LIMIT 9";
 $sql3 = "SELECT title, dateOf, sermonId FROM Sermons ORDER BY dateOf";
 $result = $link->query($sql3);
 $test = mysqli_fetch_all($result, MYSQLI_ASSOC);
-
-$json = json_encode($test);
-
-if ($json)
-    echo $json;
-else
-    echo json_last_error_msg();
 
 if (!empty($test)){
     echo json_encode($test);
