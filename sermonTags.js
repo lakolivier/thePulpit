6300619be77427dbf75e0fd7750fd3cabba45e07
lakolivier <lakolivier@gmail.com>
2020-03-14 15:06:20 -0400

    making sermonTags a reality

--- sermonTags.js
+++ sermonTags.js
@@ -12,18 +12,20 @@ $(function() {
         let lastTag = trimTag;
         $("#allTags").append('<div class = "card col-8 my-3 border-0 mx-auto" id = "' + trimTag + '"></div>');
         $("#" + trimTag).append('<h2 class = "card-title margin-0 border-bottom"><a href = "sermonTags.html#' + trimTag + '" class = "text-dark text-decoration-none">' + jsArr[0]["tagName"] + '</a></h2>');
-        console.log(jsArr.length);
         for (let i = 0; i < $(jsArr).length; i++) {
+            console.log("hello sir");
             jsArr[i]["dateOf"] = new Date(jsArr[i]["dateOf"]);
             trimTag = jsArr[i]["tagName"].replace(/\s/g, "");
             //if tag of current sermon is the same tag, append title and date
             if (jsArr[i]["tagName"].replace(/\s/g, "") == trimTag) {
+                console.log("hello sir");
                     $("#" + trimTag).append('<div class = "card my-1 bg-light border-0" id = "' + trimTag + i + '"><div class = "card-body"></div></div>');
                     $("#" + trimTag + i).children("div.card-body").append('<h4 class = "card-title text-primary">' + jsArr[i]["title"] + "</h4>");
                     $("#" + trimTag + i).children("div.card-body").append('<p class = "card-text text-muted">' + jsArr[i]["dateOf"].toLocaleDateString('en-US', options) + "</p>");
             }
             //else change holdDate to new year, make new div, append current sermon info
             else {
+                console.log("hello sir");
                 trimTag = jsArr[i]["tagName"].replace(/\s/g, "");
                 $("#" + lastTag).after('<div class = "card col-xl-12 my-3 border-0 divYear" id = "' + trimTag + '"></div>');
                 $("#" + trimTag).append('<h2 class = "card-title">' + jsArr[i]["tagName"] + "</h2>")
