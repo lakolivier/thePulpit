bf9c6b0da4b660d57ce15c921904a62d354a3886
lakolivier <lakolivier@gmail.com>
2020-03-17 18:05:06 -0400

    onHover to justDate

--- justTags.js
+++ justTags.js
@@ -6,11 +6,11 @@ $(function() {
         let trimTag = jsArr[0]["tagName"].replace(/\s/g, "");
         let lastTag = trimTag;
         $("#allTags").append('<div class = "card bg-light rounded-0 border-0" id = "' + trimTag + '"></div>');
-        $("#" + trimTag).append('<h2 class = "card-title m-0 px-3 hovElement"><a href = "sermonTags.html#' + trimTag + '" class = "text-dark text-decoration-none">' + jsArr[0]["tagName"] + '</a></h2>');
+        $("#" + trimTag).append('<h2 class = "card-title m-0 px-3 py-1 hovElement"><a href = "sermonTags.html#' + trimTag + '" class = "text-dark text-decoration-none">' + jsArr[0]["tagName"] + '</a></h2>');
         for (let i = 1; i < $(jsArr).length; i++) {
             trimTag = jsArr[i]["tagName"].replace(/\s/g, "");
             $("#" + lastTag).after('<div class = "card bg-light rounded-0 border-0" id = "' + trimTag + '"></div>');
-            $("#" + trimTag).append('<h2 class = "card-title m-0 px-3 hovElement"><a href = "sermonTags.html#' + trimTag + '" class = "text-dark text-decoration-none">' + jsArr[i]["tagName"] + '</a></h2>');
+            $("#" + trimTag).append('<h2 class = "card-title m-0 px-3 py-1 hovElement"><a href = "sermonTags.html#' + trimTag + '" class = "text-dark text-decoration-none">' + jsArr[i]["tagName"] + '</a></h2>');
             lastTag = trimTag;
         }
     });
