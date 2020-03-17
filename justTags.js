9666193b1d6cac778d0bbd4f55cdd51c1db08fc4
lakolivier <lakolivier@gmail.com>
2020-03-17 18:17:55 -0400

    adjusting justTags onhover

--- justTags.js
+++ justTags.js
@@ -6,7 +6,7 @@ $(function() {
         let trimTag = jsArr[0]["tagName"].replace(/\s/g, "");
         let lastTag = trimTag;
         $("#allTags").append('<div class = "card bg-light rounded-0 border-0" id = "' + trimTag + '"></div>');
-        $("#" + trimTag).append('<h2 class = "card-title m-0 px-3 py-1 hovElement"><a href = "sermonTags.html#' + trimTag + '" class = "text-dark text-decoration-none">' + jsArr[0]["tagName"] + '</a></h2>');
+        $("#" + trimTag).append('<a href = "sermonTags.html#' + trimTag + '" class = "text-dark text-decoration-none"><h2 class = "card-title m-0 px-3 py-1 hovElement">' + jsArr[0]["tagName"] + '</h2></a>');
         for (let i = 1; i < $(jsArr).length; i++) {
             trimTag = jsArr[i]["tagName"].replace(/\s/g, "");
             $("#" + lastTag).after('<div class = "card bg-light rounded-0 border-0" id = "' + trimTag + '"></div>');
