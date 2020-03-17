6067224c5a553bf92fcd3a7eb90b6ccd6e4e122f
lakolivier <lakolivier@gmail.com>
2020-03-17 18:18:43 -0400

    adjusting justTags onhover

--- justTags.js
+++ justTags.js
@@ -10,7 +10,7 @@ $(function() {
         for (let i = 1; i < $(jsArr).length; i++) {
             trimTag = jsArr[i]["tagName"].replace(/\s/g, "");
             $("#" + lastTag).after('<div class = "card bg-light rounded-0 border-0" id = "' + trimTag + '"></div>');
-            $("#" + trimTag).append('<h2 class = "card-title m-0 px-3 py-1 hovElement"><a href = "sermonTags.html#' + trimTag + '" class = "text-dark text-decoration-none">' + jsArr[i]["tagName"] + '</a></h2>');
+            $("#" + trimTag).append('<a href = "sermonTags.html#' + trimTag + '" class = "text-dark text-decoration-none"><h2 class = "card-title m-0 px-3 py-1 hovElement">' + jsArr[i]["tagName"] + '</h2></a>');
             lastTag = trimTag;
         }
     });
