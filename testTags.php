a6ec84246550c978a01ff1ec9024cbb4374e08b3
lakolivier <lakolivier@gmail.com>
2020-04-14 15:57:16 -0400

    testTags

--- testTags.php
+++ testTags.php
@@ -49,7 +49,7 @@ foreach ($assoc as $sermon){
         $stags[$thisSermon]["tagId"] = $sermon["tagId"];
     }
 }
-echo json_encode($stags);
+echo $stags;
 /* appearance of stags:
 stags[sermonId][sermonTag]
 first check the array for $sermons's sermon
