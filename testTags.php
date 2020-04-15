9056d9df0004b8d8795a0fd96acdcd8b410d4456
lakolivier <lakolivier@gmail.com>
2020-04-15 14:42:48 -0400

    adding tag tags on tags

--- testTags.php
+++ testTags.php
@@ -49,15 +49,13 @@ foreach ($assoc as $sermon){
         $stags[$thisTag]["sermonId"] = $sermon["sermonId"];
     }
 }*/
-$stagsI = 0;
 foreach ($assoc as $sermon){
     if (in_array($sermon["sermonId"], $stags)){
         $stags[$sermon["sermonId"]][] = $sermon["tagName"];
     }
     else{
-        $stags[$stagsI] = $sermon["sermonId"];
+        $stags[] = $sermon["sermonId"];
         $stags[$sermon["sermonId"]][] = $sermon["tagName"];
-        $stagsI++;
     }
 }
 echo json_encode($stags);
